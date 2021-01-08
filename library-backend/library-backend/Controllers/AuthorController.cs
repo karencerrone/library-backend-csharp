c314d37d16974a953ad1ae84667e4a86022cf28a
Karen Cerrone <kacerrone@gmail.com>
2021-01-07 22:54:05 -0300

    Initial commit

---
+++ library-backend/library-backend/Controllers/AuthorController.cs
@@ -0,0 +1,45 @@
+﻿using library_backend.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+
+namespace library_backend.Controllers
+{
+    [RoutePrefix("api/author")]
+    public class AuthorController : ApiController
+    {
+        // GET: api/Author
+        public Author Get()
+        {
+            Book book1 = new Book(1, "Libro1", true, new DateTime(2020, 08, 25));
+            Book book2 = new Book(1, "Libro2", false, new DateTime(2020, 10, 20));
+            Author author = new Author(1, "Author1", new List<Book>() { book1, book2 });
+            return author;
+        }
+
+        // GET: api/Author/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Author
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Author/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Author/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ library-backend/library-backend/Models/Author.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace library_backend.Models
+{
+    public class Author
+    {
+        private int id;
+        private string name;
+        private List<Book> books;
+
+        public Author()
+        {
+        }
+
+        public Author(int id, string name, List<Book> books)
+        {
+            this.id = id;
+            this.name = name;
+            this.books = books;
+        }
+
+        public int Id { get => id; set => id = value; }
+        public string Name { get => name; set => name = value; }
+        public List<Book> Books { get => books; set => books = value; }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
